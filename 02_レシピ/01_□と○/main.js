655e943cf40aa06c86ea56924137221d409efcbc
kreakdone <kreakdone@gmail.com>
2021-07-14 09:56:31 +0900

    [と]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -3,13 +3,9 @@ const figure = document.getElementById("figure")
 figure.onclick = function() {
   figure.classList.toggle("rounded")
 }
-const squareButton = document.getElementById("square-button")
-const circleButton = document.getAnimations("circle-button")
 
-circleButton.onclick = function() {
-  figure.classList.add("rounded")
-}
+const figureNameraka = document.getElementById("figureNameraka")
 
-squareButton.onclick = function() {
-  figure.classList.remove("rounded")
+figureNameraka.onclick = function() {
+  figureNameraka.classList.toggle("rounded")
 }
