737e6381ceca06a743901164a631ce807e564974
kreakdone <kreakdone@gmail.com>
2021-07-14 09:43:15 +0900

    week2の内容

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,3 +1,15 @@
 const figure = document.getElementById("figure")
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
+}
 const squareButton = document.getElementById("square-button")
-const sircleButton = document.getAnimations("circle-button")
+const circleButton = document.getAnimations("circle-button")
+
+circleButton.onclick = function() {
+  figure.classList.add("rounded")
+}
+
+squareButton.onclick = function() {
+  figure.classList.remove("rounded")
+}
