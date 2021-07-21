219b8b825de2de3192e20076ee912b96e26e5e37
kreakdone <kreakdone@gmail.com>
2021-07-21 19:49:19 +0900

    [Trello (保存機能なし)]を学習しました。

--- 02_レシピ/02_Trello（保存機能あり）/main.js
+++ 02_レシピ/02_Trello（保存機能あり）/main.js
@@ -3,7 +3,13 @@ const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
 addButton.onclick = function() {
-  const text = inputElement.value
+  const card = createCard(inputElement.value)
+  container.append(card)
+
+  inputElement.value = ""
+}
+
+const createCard = function(text) {
   const card = document.createElement("div")
   card.className = "card"
 
@@ -11,14 +17,24 @@ addButton.onclick = function() {
   todo.className = "todo"
   todo.textContent = text
   card.append(todo)
-  container.append(card)
-  inputElement.value = ""
+
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+
+  deleteButton.onclick = function() {
+    card.remove()
+  }
+  card.append(deleteButton)
+
+  return card
 }
 
 const card = document.createElement("div")
 card.className = "card"
+
 const todo = document.createElement("div")
 todo.className = "todo"
 todo.textContent = text
+
 card.append(todo)
 container.append(card)
